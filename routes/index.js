0fa6f86b77fff49dff15071a1e57f3ba7d6c0163
rogersa <roger_s_a@yahoo.es>
2015-07-30 16:19:02 -0400

    Adicionando author

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,7 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+router.get('/author',function(req, res) {
+  res.render('author', { nom_author: 'Roger Soto Aiza' });
+});
 module.exports = router;
