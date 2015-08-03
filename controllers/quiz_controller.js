813392f6c57674fda9ede77b283c599947480422
rogersa <roger_s_a@yahoo.es>
2015-08-03 18:47:45 -0400

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,28 @@
 var models = require('../models/models.js');
 
-exports.question = function(req,res){
-models.Quiz.findAll().then(function(quiz){
- res.render('quizes/question',{pregunta:quiz[0].pregunta});	
+exports.show = function(req,res){
+models.Quiz.find(req.params.quizId).then(function(quiz){
+ res.render('quizes/show',{quiz : quiz});	
 })
 
 };
 exports.answer = function(req,res){
- models.Quiz.findAll().then(function (quiz){
-      if(req.query.respuesta === quiz[0].respuesta)
+ models.Quiz.find(req.params.quizId).then(function (quiz){
+      if(req.query.respuesta === quiz.respuesta)
 	  {
-		  res.render('quizes/answer',{respuesta:'Correcto'});
+		  res.render('quizes/answer',{quiz:quiz,respuesta:'Correcto'});
 	  }
       else
 	  {
-		  res.render('quizes/answer',{respuesta:'Incorrecto'});
+		  res.render('quizes/answer',{quiz:quiz,respuesta:'Incorrecto'});
 	  }
      
  })		    
+};
+
+exports.index = function(req,res){
+models.Quiz.findAll().then(function(quizes){ 
+    res.render('quizes/index.ejs',{quizes:quizes});
+})
+
 };
