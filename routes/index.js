26b48841ed0b2419b1967a270e305cce849f41ee
rogersa <roger_s_a@yahoo.es>
2015-08-08 19:10:46 -0400

    Autenticacion y session

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var quizController= require('../controllers/quiz_controller');
-var commentController= require('../controllers/comment_controller');
+var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,6 +10,12 @@ router.get('/', function(req, res) {
 });
 
 router.param('quizId',quizController.load);
+
+// Definicion de rutas de session
+
+router.get('/login',sessionController.new);
+router.post('/login',sessionController.create);
+router.get('/logout',sessionController.destroy);
 
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
